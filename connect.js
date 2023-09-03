b1a1c2addd627c4f3fcc5545608f4d9f8caf7026
Hemaet33 <hemaet3333@gmail.com>
2023-09-03 23:53:33 +0600

    Hello

--- connect.js
+++ connect.js
@@ -1,17 +1,11 @@
 import mysql from 'mysql';
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST, 
-    user: process.env.DB_USER, 
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+
+export const db = mysql.createConnection({
+  host:process.env.DB_HOST,
+  user:process.env.DB_USER,
+  password:process.env.DB_PASSWORD,
+  database:process.env.DB_NAME
 });
 
-export const db = pool.getConnection((err, conn) => {
-    if(err) console.log(err)
-    console.log("Connected successfully")
-})
 
