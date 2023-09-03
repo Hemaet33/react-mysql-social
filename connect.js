5d0976efc2c9c2101f07b1c99a753ba227797193
Hemaet33 <hemaet3333@gmail.com>
2023-09-03 23:07:19 +0600

    Hello

--- connect.js
+++ connect.js
@@ -4,7 +4,7 @@ import mysql from 'mysql';
 export const db = mysql.createConnection({
   host:process.env.DB_HOST,
   user:process.env.DB_USER,
-  password:"password",
+  password:"",
   database:process.env.DB_NAME,
   port:process.env.PORT
 });
