237516f017c9378b17775fb50cabeac45dd5f663
Hemaet33 <hemaet3333@gmail.com>
2023-09-05 22:49:06 +0600

    changes

--- index.js
+++ index.js
@@ -17,12 +17,19 @@ app.use((req, res, next)=>{
   res.header("Access-Control-Allow-Credentials", true);
   next()
 });
-
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173/"
+  origin: "http://localhost:5173"
 }));
 app.use(cookieParser());
 
