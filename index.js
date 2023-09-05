1d1f43a9af87e7cd5d19b88d4f7277daa5b90d0a
Hemaet33 <hemaet3333@gmail.com>
2023-09-05 22:33:03 +0600

    changes

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use((req, res, next)=>{
   next()
 });
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://jovial-bienenstitch-6b8e36.netlify.app");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173/");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
