e8fbff9382ebd4cbca4ea7d3bd2de8cffbdee65e
Hemaet33 <hemaet3333@gmail.com>
2023-09-05 23:47:52 +0600

    changes

--- controllers/auth.js
+++ controllers/auth.js
@@ -51,7 +51,9 @@ export const login = (req, res)=>{
   
       res.cookie("accessToken", token, {
         expires:  new Date(Date.now()+(3*24*60*60*1000)),
-        httpOnly:true
+        httpOnly:true,
+        sameSite:"none",
+        secure:true
       }).status(200).json(others); 
   
     });
