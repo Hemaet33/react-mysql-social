5039475ac684b21cfe42e8ffda1140dee068d76e
Hemaet33 <hemaet3333@gmail.com>
2023-09-05 22:45:32 +0600

    changes

--- index.js
+++ index.js
@@ -17,14 +17,7 @@ app.use((req, res, next)=>{
   res.header("Access-Control-Allow-Credentials", true);
   next()
 });
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173/");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+
 
 
 app.use(express.json());
