67d5e099c2969141b16120982348e0f2da92f36b
Hemaet33 <hemaet3333@gmail.com>
2023-09-07 18:33:08 +0600

    changes

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.use('/api/auth', authRoutes);
 
 const storage = multer.diskStorage({
   filename: function(req, file, cb){
-    cb(null, file.originalname)
+    return cb(null, file.originalname)
   }
 })
 
