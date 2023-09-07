c7cc2c3676f7dc286c7e947f6226a818bd6f17bb
Hemaet33 <hemaet3333@gmail.com>
2023-09-07 19:29:38 +0600

    changes

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ app.post('/api/upload', upload.single('file'),(req,res)=>{
   //     data:result
   //   });
   // });
-  res.status(200).json({path:req.file.path}) 
+  res.status(200).json({paths:req.file.path}) 
 })
 
 app.listen(port,()=>{
