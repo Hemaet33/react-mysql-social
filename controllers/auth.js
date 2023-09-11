e9c207f99619b2a961e0671e769d3ed0ade52def
Hemaet33 <hemaet3333@gmail.com>
2023-09-11 16:58:55 +0600

    changes

--- controllers/auth.js
+++ controllers/auth.js
@@ -45,7 +45,7 @@ export const login = (req, res)=>{
   
       if(!checkPassword) return res.status(400).json("Wrong password or username!")
   
-      const token = jwt.sign({id:data[0].id}, "secretKey",{expiresIn:"3d"});
+      const token = jwt.sign({id:data[0].id}, "secretKey");
   
       const {password, ...others} = data[0];
   
