14f7d8935d33b68c66027aeb003452477b8d208d
Hemaet33 <hemaet3333@gmail.com>
2023-09-14 16:36:25 +0600

    changes

--- controllers/users.js
+++ controllers/users.js
@@ -52,10 +52,11 @@ export const getUsers = (req, res)=>{
   jwt.verify(token, "secretKey",(err, userInfo)=>{
     if(err) return res.status(403).json("Invalid token!");
 
-  const q = "SELECT users.id,users.profilePic,users.name FROM users JOIN relationships ON(users.id = relationships.followedUserId) WHERE relationships.followerUserId=?";
+  const q = "SELECT users.id,users.profilePic,users.name FROM users MINUS SELECT users.id,users.profilePic,users.name FROM users JOIN relationships ON(users.id = relationships.followedUserId) WHERE relationships.followerUserId=? AND users.id=?";
 
   const values = [
-    userInfo.id
+    userInfo.id,
+    userInfo.id,
   ]
 
     db.query(q, values, (err, data)=>{
